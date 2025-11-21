c76d3eb2ea2cb0241692c266a8c30b2104e9e50d
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2025-11-21 21:15:37 +0000

    Initialised terraform and created the files

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ecs-project-s3"
+    key = "terraform.tfstate"
+    region = "eu-north-1"
+    use_lockfile = true #s3 native locking 
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
